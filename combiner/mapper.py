e530f429b069094cecd5dda7b056bb5084078e1c
Belal <billy.fcih@yahoo.com>
2017-12-14 15:39:32 +0200

    Map the two datasets

--- combiner/mapper.py
+++ combiner/mapper.py
@@ -2,7 +2,7 @@
 import sys
 
 
-def _format_and_split(line, separator='\t'):
+def _format_and_split(line, separator=','):
     # remove leading and trailing whitespace and split on separator
     return line.strip().split(separator)
 
@@ -19,19 +19,21 @@ def _emit(elements, separator='\t'):
 def mapper():
 
     for line in sys.stdin:
-        movie_id = "-"
-        country = "-"
+        movie_title = "-"
+        user_id = "-"
+        ratings = "-"
 
         splits = _format_and_split(line)
 
-        if len(splits) == 5:
-            user_id = splits[0]
-            country = splits[4]
+        if len(splits) == 3:
+            movie_id = splits[0]
+            movie_title = splits[1]
         else:
-            user_id = splits[1]
-            movie_id = splits[2]
+            movie_id = splits[1]
+            user_id = splits[0]
+            ratings = splits[2]
 
-        _emit([user_id, movie_id, country])
+        _emit([movie_id, movie_title, ratings])
 
 
 if __name__ == '__main__':
